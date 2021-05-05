15ddf28a1ce6eff137688e0a106a33cda5077da7
LorenzoCeccarelli <56482293+LorenzoCeccarelli@users.noreply.github.com>
2021-05-05 15:12:43 +0200

    init project

---
+++ src/main/kotlin/it/polito/wa2group8/warehousewebflux/WarehouseWebFluxApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2group8.warehousewebflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseWebFluxApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarehouseWebFluxApplication>(*args)
+}
---
+++ src/test/kotlin/it/polito/wa2group8/warehousewebflux/WarehouseWebFluxApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2group8.warehousewebflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseWebFluxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

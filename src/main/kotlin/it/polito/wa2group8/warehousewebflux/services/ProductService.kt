db9491d68c9e6ffb08863fcd9a1dfa5fca0bda22
Riccardo Tedesco <42493739+ricky96highlander@users.noreply.github.com>
2021-05-07 12:20:52 +0200

    Service: getProductByID and getAllProducts (first version)

--- src/main/kotlin/it/polito/wa2group8/warehousewebflux/services/ProductService.kt
+++ src/main/kotlin/it/polito/wa2group8/warehousewebflux/services/ProductService.kt
@@ -1,6 +1,7 @@
 package it.polito.wa2group8.warehousewebflux.services
 
 import it.polito.wa2group8.warehousewebflux.dto.ProductDTO
+import kotlinx.coroutines.flow.Flow
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -9,8 +10,8 @@ interface ProductService {
 
     suspend fun createProduct(productDTO: ProductDTO): ProductDTO
     suspend fun updateProduct(productDTO: ProductDTO): ProductDTO
-    fun retrieveProduct(id: Long): ProductDTO
-    fun retrieveAllProducts(): Flux<ProductDTO>
+    suspend fun retrieveProduct(id: Long): ProductDTO
+    suspend fun retrieveAllProducts(): Flow<ProductDTO>
     fun retrieveProductsByCategory(category: String): Flux<ProductDTO>
 
 }
--- src/main/kotlin/it/polito/wa2group8/warehousewebflux/services/ProductServiceImpl.kt
+++ src/main/kotlin/it/polito/wa2group8/warehousewebflux/services/ProductServiceImpl.kt
@@ -2,7 +2,10 @@ package it.polito.wa2group8.warehousewebflux.services
 
 import it.polito.wa2group8.warehousewebflux.domain.Product
 import it.polito.wa2group8.warehousewebflux.dto.ProductDTO
+import it.polito.wa2group8.warehousewebflux.exceptions.NotFoundException
 import it.polito.wa2group8.warehousewebflux.repositories.ProductRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Flux
@@ -29,12 +32,13 @@ class ProductServiceImpl(
 
     }
 
-    override fun retrieveProduct(id: Long): ProductDTO {
-        TODO("Not yet implemented")
+    override suspend fun retrieveProduct(id: Long): ProductDTO {
+        val product = productRepository.findById(id) ?: throw NotFoundException("Product not found")
+        return product.toProductDTO()
     }
 
-    override fun retrieveAllProducts(): Flux<ProductDTO> {
-        TODO("Not yet implemented")
+    override suspend fun retrieveAllProducts(): Flow<ProductDTO> {
+        return productRepository.findAll().map { t -> t.toTransactionDTO() } //?: throw NotFoundException("Product not found")
     }
 
     override fun retrieveProductsByCategory(category: String): Flux<ProductDTO> {
